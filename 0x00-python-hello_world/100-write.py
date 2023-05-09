7d92256d25644ff5631e6f6d3a457272fd48c989
ziggybaz <77249316+ziggybaz@users.noreply.github.com>
2023-05-09 18:58:03 +0300

    Update 100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 import sys
-sys.stderr.write(and that piece of art is useful - Dora Korpar, 2015-10-19n\)
+
+string = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(string)
 sys.exit(1)
