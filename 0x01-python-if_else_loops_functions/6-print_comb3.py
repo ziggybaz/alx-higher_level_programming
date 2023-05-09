db7d46fae71b596bb271de64906c7406af173a29
ziggybaz <77249316+ziggybaz@users.noreply.github.com>
2023-05-09 18:35:17 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for n in range(0, 10):
     for i in range((n+1), 10):
-        if (n is not 8) or (i is not 9):
+        if (n != 8) or (i != 9):
             print("{}{}, ".format(n, i), end="")
         else:
             print("{}{}".format(n, i))
