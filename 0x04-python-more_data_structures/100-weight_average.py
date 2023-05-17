e1ba62a4f7c0d54b512ff78a01fd659358bc4df3
ziggybaz <77249316+ziggybaz@users.noreply.github.com>
2023-05-17 22:00:39 +0300

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list is not None:
-        total = 0
-        frequency = 0
-        for tup in my_list:
-            (weight, occurence) = tup
-            total += (weight * occurence)
-            frequency += occurence
-        return (total/frequency) if frequency > 0 else 0
+        sum = 0
+        regularity = 0
+        for i in my_list:
+            (weight, occurence) = i
+            sum += (weight * occurence)
+            regularity += occurence
+        return (sum/regularity) if regularity > 0 else 0
     else:
         return (0)
